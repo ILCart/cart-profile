311bfe0f6eac2f6aaf0ab0a989e2794af894cf89
ILCart <143832706+ILCart@users.noreply.github.com>
2023-09-06 10:01:35 -0700

    Bathroom counter

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const theButton = document.getElementById('button');
 const panels = document.querySelectorAll('.panel');
 const closeButton = document.getElementById('close');
 const FollowButton = document.getElementById('Flw');
-
+const BathroomCounter = document.getElementById("BrCounter")
 
 // Add the event listener to the profile button
 theButton.addEventListener('click', () => {
@@ -24,3 +24,12 @@ closeButton.addEventListener('click', () => {
 FollowButton.addEventListener('click', () => {
   window.open("https://github.com/ILCart/cart-profile").focus()
 });
+
+// Add the event listener to the follow button
+FollowButton.addEventListener('click', () => {
+  window.open("https://github.com/ILCart/cart-profile").focus()
+});
+const Today = new Date()
+const Start = new Date('August 25, 2023 0:0:0')
+const Diff = Today - Start
+BathroomCounter.textContent = parseInt(Diff/(1000*60*60*24))
